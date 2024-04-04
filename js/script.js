d2878fef906e44db478cf39fa03e2d238068e4e0
Marcos Paulo <marcos.paulo.s.m.filho@gmail.com>
2024-04-04 10:22:13 -0300

    fix: removendo categoria da tarefa dos campos

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@
 const tarefaForm = document.getElementById('form-tarefa');
 const tituloInput = document.getElementById('titulo');
 const descricaoInput = document.getElementById('descricao');
-const categoriaInput = document.getElementById('categoria');
 const dataInput = document.getElementById('data');
 
 const listaTarefas = document.getElementById('lista-tarefas-body');
